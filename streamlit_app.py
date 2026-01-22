c098cfc6f86f6a409b76d57dc895d14fe587a3ff
Damian Gasior <damian.piotr.gasior@gmail.com>
2026-01-22 23:14:06 +0100

    adding latest changes with pages adjustments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Financial Dashboard")
-st.write("Welcome, go the the tabs on the left, start from the top👈")
+st.write("Welcome, go the the tabs on the left, start from the top left👈")
